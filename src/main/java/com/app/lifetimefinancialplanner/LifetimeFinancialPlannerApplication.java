5fac5907ec208dc75d4f273d8221786920caba0a
손성민 <alexzzanga7407@gmail.com>
2025-02-05 20:09:13 -0500

    Initial project setup

---
+++ src/main/java/com/app/lifetimefinancialplanner/LifetimeFinancialPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.app.lifetimefinancialplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifetimeFinancialPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifetimeFinancialPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/lifetimefinancialplanner/LifetimeFinancialPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.lifetimefinancialplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifetimeFinancialPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
